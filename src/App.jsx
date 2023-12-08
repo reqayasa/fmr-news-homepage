f9a64ac1115a63bfd9966db59026b86ead4ac6f4
reqayasa <reqayasateknik@gmail.com>
2023-12-08 09:05:31 +0700

    Set requerment style guides

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import './App.css';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-sky-500">
+    <h1 className="text-3xl text-center w-full m-10 font-bold underline text-sky-500">
       Hello world!
     </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter'],
+      },
+      colors: {
+        soft_orange: {
+          500: 'hsl(35 77 62 / 1)',
+        },
+        soft_red: {
+          500: 'hsl(5 85 63 / 1)',
+        },
+        off_white: {
+          500: 'hsl(36 100 99 / 1)'
+        },
+        grayish_blue: {
+          500: 'hsl(233 8 79 / 1)'
+        },
+        dark_grayish_blue: {
+          500: 'hsl(236 13 42 / 1)'
+        },
+        very_dark_blue: {
+          500: 'hsl(240 10 5 / 1)'
+        },
+      }
+    },
   },
   plugins: [],
 }
