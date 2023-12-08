c904b59f37eb7afd3b4f842b89210fcad7a9b483
reqayasa <reqayasateknik@gmail.com>
2023-12-08 09:35:57 +0700

    fix: Syntax error in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,22 +11,22 @@ export default {
       },
       colors: {
         soft_orange: {
-          500: 'hsl(35 77 62 / 1)',
+          500: 'hsl(35deg 77% 62% / 1)',
         },
         soft_red: {
-          500: 'hsl(5 85 63 / 1)',
+          500: 'hsl(5deg 85% 63% / 1)',
         },
         off_white: {
-          500: 'hsl(36 100 99 / 1)'
+          500: 'hsl(36deg 100% 99% / 1)'
         },
         grayish_blue: {
-          500: 'hsl(233 8 79 / 1)'
+          500: 'hsl(233deg 8% 79% / 1)'
         },
         dark_grayish_blue: {
-          500: 'hsl(236 13 42 / 1)'
+          500: 'hsl(236deg 13% 42% / 1)'
         },
         very_dark_blue: {
-          500: 'hsl(240 10 5 / 1)'
+          500: 'hsl(240deg 10% 5% / 1)'
         },
       }
     },
