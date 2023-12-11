857c1121c9a0039bde289a817204b331d0aa6661
reqayasa <reqayasateknik@gmail.com>
2023-12-11 16:41:07 +0700

    Add atribution

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Header from './components/Header';
 import TopStories from './components/TopStories';
 import NewNewsList from './components/NewNewsList';
 import Feed from './components/Feed';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div className='max-w-[1110px] sm:mx-4 lg:mx-auto '>
       <Header />
-      <div className='md:grid md:grid-cols-3 md:grid-flow-row gap-[30px]'>
-        <TopStories className='md:col-span-2' />
-        <NewNewsList className='' />
-      </div>
-      <Feed />
+      <main>
+        <div className='md:grid md:grid-cols-3 md:grid-flow-row gap-[30px]'>
+          <TopStories className='md:col-span-2' />
+          <NewNewsList className='' />
+        </div>
+        <Feed />
+      </main>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer
+      className="mt-10 mb-2 flex w-full justify-center text-xs text-grayish_blue-500"
+    >
+      Challenge by &nbsp;
+      <a
+          className="hover:text-very_dark_blue-500 text-dark_grayish_blue-500"
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+          >Frontend Mentor</a
+      >. Coded by &nbsp;
+      <a
+          className="hover:text-very_dark_blue-500 text-dark_grayish_blue-500"
+          href="https://github.com/reqayasa/fmr-faq-accordion-main"
+          target="_blank"
+          rel="noreferrer"
+          >Reqayasa</a
+      >.
+    </footer>
+  )
+}
